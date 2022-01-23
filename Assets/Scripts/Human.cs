d6e58297b02eea921bdace7a3bd06c0e26db0496
Enes <menesalnet@gmail.com>
2022-01-23 17:42:50 +0300

    added functions to human cs

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -35,7 +35,7 @@ public class Human : MonoBehaviour
 
     private float pickCubePosition = 0;
     private int count = 0;
-    private int v;
+    private int stairsCubeCount;
     private int imageCount;
     public float stairsCount = 0;
     public float stairsCount2 = 0;
@@ -48,35 +48,26 @@ public class Human : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        imageCount = 0;
+        ParticleFunction();
+        
         thisAnimator = gameObject.GetComponent<Animator>();
         parentSpeed = GameObject.FindObjectOfType<HumanParent>();
-        particlePosition = victoryParticle.transform.position;
-        particlePosition2 = victoryParticle2.transform.position;
-        particlePosition3 = victoryParticle3.transform.position;
-        particlePosition4 = victoryParticle4.transform.position;
-        victoryParticle.transform.position = new Vector3(10f, 10f, 10f);
-        victoryParticle2.transform.position = new Vector3(10f, 10f, 10f);
-        victoryParticle3.transform.position = new Vector3(10f, 10f, 10f);
-        victoryParticle4.transform.position = new Vector3(10f, 10f, 10f);
-        victoryParticle.Pause();
-        victoryParticle2.Pause();
-        victoryParticle3.Pause();
-        victoryParticle4.Pause();
-        imageCount = 0;
-        cam = Camera.main;
         pick = new List<GameObject>();
+        cam = Camera.main;
         
-        Debug.Log("fark" + (cam.transform.position.y - transform.position.y));
+        
     }
     void Update() {
         
         if(stairsUp == true) {
 
             cam.transform.position = Vector3.Lerp(cam.transform.position,new Vector3(cam.transform.position.x, camPosition.y ,cam.transform.position.z),.2f*Time.deltaTime);
-            //camPosition = cam.transform.position;
+            
         }
         Debug.Log("listedeki eleman say�s�"+pick.Count);
         if(finishControl == false) {
+            
             if(Input.touchCount <= 1) {
                 
                 if(Input.GetMouseButtonDown(0)) {
@@ -111,147 +102,211 @@ public class Human : MonoBehaviour
     
     
     private void OnTriggerEnter(Collider other) {
+        
         if(other.tag == "pickup") {
 
-            pickCubePosition += 0.2f;
-            count++;
-            //Debug.Log("selam");
-            thisAnimator.SetBool("pick", true);
-            thisAnimator.SetBool("return", false);
-            other.gameObject.transform.SetParent(pickParent.transform);
-            other.transform.localPosition = new Vector3(0, pickCubePosition, 0);
-            //ParticleSystem pickSystem = Instantiate(victoryParticle2, other.transform);
-            other.gameObject.GetComponent<Collider>().enabled = false;
-            pick.Add(other.gameObject);
-
-            //Debug.Log(count);
+            PickUpFunction(other);                                  
             
         }
-        if(other.tag == "answer1") {
-            //Debug.Log("sea");
+        else if(other.tag == "answer1") {
             
-            for(int j = 0; j < 3; j++) {
-                pickCubePosition += 0.2f;
-                GameObject newObject = Instantiate(pickObject);
-                thisAnimator.SetBool("pick", true);
-                thisAnimator.SetBool("return", false);
-                newObject.gameObject.transform.SetParent(pickParent.transform);
-                newObject.transform.localPosition = new Vector3(0, pickCubePosition, 0);
-                newObject.GetComponent<Collider>().enabled = false;
-                pick.Add(newObject);
-                
-                //Destroy(other.GetComponentInParent<GameObject>());
-                //Debug.Log(pick.Count);
-
-            }
-            
-            if(imageCount < 4 ) {
-                images[imageCount].SetActive(false);
-                imageCount++;
-            }
-            other.transform.parent.gameObject.SetActive(false);
+            TrueAnswerFunction(other);
 
         }
         else if(other.tag == "answer2") {
-            other.gameObject.GetComponentInParent<Transform>().position = new Vector3(0, 0, 0);
-            if(pick.Count >= 3) {
-                
-                for(int i = 1; i < 4; i++) {
-                    pick[pick.Count - 1].SetActive(false);
-                    pick[pick.Count - 1].transform.SetParent(null);
-                    pick.Remove(pick[pick.Count - 1]);
-                    if(pick.Count == 0) {
-                        thisAnimator.SetBool("return", true);
-                        thisAnimator.SetBool("pick", false);
-                    }
-                    pickCubePosition -= 0.2f;
-                }
-                            
-            }
-            if(pick.Count == 0) {
-                thisAnimator.SetBool("return", true);
-                thisAnimator.SetBool("pick", false);
-                Debug.Log("listeBo�");
-            }
-
-            if(imageCount < 4) {
-                images[imageCount].SetActive(false);
-                imageCount++;
-            }
-            other.transform.parent.gameObject.SetActive(false);
+            
+            FalseAnswerFunction(other);
 
         }
         else if(other.tag == "stairs") {
-            if(touched == false) {
-                if(pick.Count > 7) {
-                    v = 7;
-                }
-                else {
-                    v = pick.Count;
-                }
-                touched = true;
-            }
-                
-            for (int c = 0; c < v; c++) {
-
-                Debug.Log("passionpunch" + pick.Count);
-                stairsCount += .3f;
-                pick[pick.Count - 1].tag = "Untagged";
-                pick[pick.Count - 1].transform.SetParent(null);
-                pick[pick.Count - 1].transform.position = new Vector3(transform.position.x,transform.position.y + 1 + stairsCount-stairsCount2-1.4f, transform.position.z+stairsCount);
-                pick[pick.Count - 1].GetComponent<Collider>().enabled = true;
-                pick[pick.Count - 1].GetComponent<Collider>().isTrigger = false;
-                pick.Remove(pick[pick.Count - 1]);
-                stairsCount2 += 0.2f;
-                pickCubePosition -= 0.2f;
-                
-                if(pick.Count == 0) {
-                    gameObject.GetComponent<Animator>().SetBool("return", true);
-                    gameObject.GetComponent<Animator>().SetBool("pick", false);
-                    Debug.Log("listeBo�");
-                }
-            }
-            camPosition = cam.transform.position + new Vector3(0, .35f, .1f);
-            //cam.transform.position += new Vector3(0, .3f, -.2f);
-            stairsUp = true;
-            stairsCount = 0;
-            stairsCount2 = 0;
-            touched = false;
+            
+            StairsFunction();
+            
         }
         else if(other.tag == "finishAnim") {
-            finishControl = true;
-            parentSpeed.speed = 0f;
-            backButton.SetActive(false);
-            foreach(GameObject cube in pick) {
-                if(cube.activeSelf == true)
-                    cube.SetActive(false);
-            }
-            thisAnimator.SetBool("victory", true);
-            victoryParticle.transform.position = particlePosition;
-            victoryParticle2.transform.position = particlePosition2;
-            victoryParticle3.transform.position = particlePosition3;
-            victoryParticle4.transform.position = particlePosition4;
-            victoryParticle.Play();
-            victoryParticle2.Play();
-            victoryParticle3.Play();
-            victoryParticle4.Play();
-            finishPanel.SetActive(true);
+            
+            FinishAnimFunction();
+            
         }
 
     }
     private void OnCollisionEnter(Collision collision) {
-       if(collision.collider.tag == "finish") {
-            backButton.transform.localScale = new Vector3(0, 0, 0);
-            tryAgainPanel.SetActive(true);
-            finishControl = true;
-            parentSpeed.speed = 0f;
-            collision.collider.GetComponent<Collider>().enabled = false;
-            foreach(GameObject cube in pick) {
-                if(cube.activeSelf == true)
-                    cube.SetActive(false);
-            }
+       if(collision.collider.tag == "sea") {
+           TryAgainFunction(collision);
         }
     }
+
+    public void ParticleFunction() {
+        
+        particlePosition = victoryParticle.transform.position;
+        particlePosition2 = victoryParticle2.transform.position;
+        particlePosition3 = victoryParticle3.transform.position;
+        particlePosition4 = victoryParticle4.transform.position;
+        
+        victoryParticle.transform.position = new Vector3(10f, 10f, 10f);
+        victoryParticle2.transform.position = new Vector3(10f, 10f, 10f);
+        victoryParticle3.transform.position = new Vector3(10f, 10f, 10f);
+        victoryParticle4.transform.position = new Vector3(10f, 10f, 10f);
+        
+        victoryParticle.Pause();
+        victoryParticle2.Pause();
+        victoryParticle3.Pause();
+        victoryParticle4.Pause();
+    }
+
+    public void PickUpFunction(Collider other) {
+        
+        pickCubePosition += 0.2f;
+        count++;
+        //Debug.Log("selam");
+        thisAnimator.SetBool("pick", true);
+        thisAnimator.SetBool("return", false);
+        other.gameObject.transform.SetParent(pickParent.transform);
+        other.transform.localPosition = new Vector3(0, pickCubePosition, 0);
+        //ParticleSystem pickSystem = Instantiate(victoryParticle2, other.transform);
+        other.gameObject.GetComponent<Collider>().enabled = false;
+        pick.Add(other.gameObject);
+
+    }
+
+    public void TrueAnswerFunction(Collider answer1) {
+        //Debug.Log("sea");
+            
+        for(int j = 0; j < 3; j++) {
+            pickCubePosition += 0.2f;
+            GameObject newObject = Instantiate(pickObject);
+            newObject.gameObject.transform.SetParent(pickParent.transform);
+            newObject.transform.localPosition = new Vector3(0, pickCubePosition, 0);
+            newObject.GetComponent<Collider>().enabled = false;
+            pick.Add(newObject);
+            
+            if (j == 1) {
+                
+                thisAnimator.SetBool("pick", true);
+                thisAnimator.SetBool("return", false);
+                
+            }
+
+        }
+            
+        if(imageCount < 4 ) {
+            images[imageCount].SetActive(false);
+            imageCount++;
+        }
+        answer1.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void FalseAnswerFunction(Collider answer2) {
+        
+        answer2.gameObject.GetComponentInParent<Transform>().position = new Vector3(0, 0, 0);
+        
+        if(pick.Count >= 3) {
+                
+            for(int i = 1; i < 4; i++) {
+                
+                pick[pick.Count - 1].SetActive(false);
+                pick[pick.Count - 1].transform.SetParent(null);
+                pick.Remove(pick[pick.Count - 1]);
+                pickCubePosition -= 0.2f;
+                
+                if(pick.Count == 0) {
+                    thisAnimator.SetBool("return", true);
+                    thisAnimator.SetBool("pick", false);
+                }
+                
+            }
+                            
+        }
+        else if(pick.Count == 0) {
+            
+            thisAnimator.SetBool("return", true);
+            thisAnimator.SetBool("pick", false);
+            Debug.Log("listeBo�");
+            
+        }
+
+        if(imageCount < 4) {
+            images[imageCount].SetActive(false);
+            imageCount++;
+        }
+        answer2.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void StairsFunction() {
+        
+        if(touched == false) {
+            if(pick.Count > 7) {
+                stairsCubeCount = 7;
+            }
+            else {
+                stairsCubeCount = pick.Count;
+            }
+            touched = true;
+        }
+                
+        for (int c = 0; c < stairsCubeCount; c++) {
+
+            Debug.Log("passionpunch" + pick.Count);
+            stairsCount += .3f;
+            pick[pick.Count - 1].tag = "Untagged";
+            pick[pick.Count - 1].transform.SetParent(null);
+            pick[pick.Count - 1].transform.position = new Vector3(transform.position.x,transform.position.y + 1 + stairsCount-stairsCount2-1.4f, transform.position.z+stairsCount);
+            pick[pick.Count - 1].GetComponent<Collider>().enabled = true;
+            pick[pick.Count - 1].GetComponent<Collider>().isTrigger = false;
+            pick.Remove(pick[pick.Count - 1]);
+            stairsCount2 += 0.2f;
+            pickCubePosition -= 0.2f;
+                
+            if(pick.Count == 0) {
+                gameObject.GetComponent<Animator>().SetBool("return", true);
+                gameObject.GetComponent<Animator>().SetBool("pick", false);
+                Debug.Log("listeBo�");
+            }
+        }
+        camPosition = cam.transform.position + new Vector3(0, .35f, .1f);
+        //cam.transform.position += new Vector3(0, .3f, -.2f);
+        stairsUp = true;
+        stairsCount = 0;
+        stairsCount2 = 0;
+        touched = false;
+    }
+
+    public void FinishAnimFunction() {
+        finishControl = true;
+        parentSpeed.speed = 0f;
+        backButton.SetActive(false);
+        thisAnimator.SetBool("victory", true);
+        victoryParticle.transform.position = particlePosition;
+        victoryParticle2.transform.position = particlePosition2;
+        victoryParticle3.transform.position = particlePosition3;
+        victoryParticle4.transform.position = particlePosition4;
+        victoryParticle.Play();
+        victoryParticle2.Play();
+        victoryParticle3.Play();
+        victoryParticle4.Play();
+        finishPanel.SetActive(true);
+        foreach(GameObject cube in pick) {
+            if(cube.activeSelf == true)
+                cube.SetActive(false);
+        }
+    }
+
+    public void TryAgainFunction(Collision tryAgain) {
+        
+        backButton.transform.localScale = new Vector3(0, 0, 0);
+        tryAgainPanel.SetActive(true);
+        finishControl = true;
+        parentSpeed.speed = 0f;
+        tryAgain.collider.GetComponent<Collider>().enabled = false;
+        
+        foreach(GameObject cube in pick) {
+            if(cube.activeSelf == true)
+                cube.SetActive(false);
+        }
+        
+    }
+    
+    
 
 
 }
